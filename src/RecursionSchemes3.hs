aade0747a3d250280ce0d05d9beea6e590321af6
Sasa Bogicevic <t4nt0r@protonmail.com>
2019-08-01 10:46:30 +0200

    Implement filter with cata

--- src/RecursionSchemes3.hs
+++ src/RecursionSchemes3.hs
@@ -8,6 +8,7 @@
 {-# LANGUAGE StandaloneDeriving  #-}
 {-# LANGUAGE TemplateHaskell     #-}
 {-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeApplications    #-}
 
 module RecursionSchemes3 where
 
@@ -26,7 +27,7 @@ makeBaseFunctor ''Test
 -- data TestF r = TestF {testAF :: [Char], testBF :: Int}
 
 testL :: [Test]
-testL = [Test "a" 1, Test "b" 2, Test "c" 3, Test "d" 4, Test "e" 5]
+testL = [Test "a" 1, Test "b" 2, Test "c" 3, Test "d" 4, Test "e" 5, Test "c" 7]
 
 foldTest :: [Test] -> Int
 foldTest = cata alg
@@ -41,3 +42,13 @@ foldTestS = cata alg
     alg :: ListF Test String -> String
     alg Nil = ""
     alg (Cons a ints) = testA a <> ints
+
+f :: Test -> Bool
+f a = testA a == "c"
+
+filterF :: forall a . (a -> Bool) -> [a] -> [a]
+filterF filterBy = cata alg
+  where
+    alg :: ListF a [a] -> [a]
+    alg  Nil = []
+    alg (Cons a as) = if filterBy a then a : as else as
