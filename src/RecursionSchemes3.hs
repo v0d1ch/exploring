fdc775187ca554ce738a1fb2008dbd8e913e3576
Sasa Bogicevic <t4nt0r@protonmail.com>
2019-08-09 00:13:54 +0200

    Ana

--- src/RecursionSchemes3.hs
+++ src/RecursionSchemes3.hs
@@ -1,18 +1,18 @@
-{-# LANGUAGE DeriveFoldable      #-}
-{-# LANGUAGE DeriveFunctor       #-}
-{-# LANGUAGE DeriveTraversable   #-}
-{-# LANGUAGE FlexibleInstances   #-}
-{-# LANGUAGE LambdaCase          #-}
-{-# LANGUAGE RankNTypes          #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE StandaloneDeriving  #-}
-{-# LANGUAGE TemplateHaskell     #-}
-{-# LANGUAGE TypeFamilies        #-}
-{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE DeriveFoldable        #-}
+{-# LANGUAGE DeriveFunctor         #-}
+{-# LANGUAGE DeriveTraversable     #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE LambdaCase            #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE StandaloneDeriving    #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
 
 module RecursionSchemes3 where
 
-import Control.Monad.Fix
 import Data.Functor.Foldable
 import Data.Functor.Foldable.TH
 
@@ -33,14 +33,14 @@ foldTest :: [Test] -> Int
 foldTest = cata alg
   where
     alg :: ListF Test Int -> Int
-    alg Nil = 0
+    alg Nil           = 0
     alg (Cons a ints) = testB a + ints
 
 foldTestS :: [Test] -> String
 foldTestS = cata alg
   where
     alg :: ListF Test String -> String
-    alg Nil = ""
+    alg Nil           = ""
     alg (Cons a ints) = testA a <> ints
 
 f :: Test -> Bool
@@ -50,7 +50,7 @@ filterF :: forall a . (a -> Bool) -> [a] -> [a]
 filterF filterBy = cata alg
   where
     alg :: ListF a [a] -> [a]
-    alg  Nil = []
+    alg  Nil        = []
     alg (Cons a as) = if filterBy a then a : as else as
 
 filterP :: forall a . Show a => (a -> Bool) -> [a] -> IO [a]
@@ -66,3 +66,11 @@ filterP filterBy = cataA alg
           return $ a:x
         else
           as
+
+anaT :: Int -> [Int]
+anaT = ana coalg
+  where
+    coalg :: Int -> Base [Int] Int
+    coalg n
+      | n >  10 = Nil
+      | otherwise = Cons n (n + 1)
