b7b7e7203a1583b12a7e70de3d4cbe7f8b43ea0e
Sasa Bogicevic <t4nt0r@protonmail.com>
2019-08-30 14:08:48 +0200

    ~

--- src/Comonadic.hs
+++ src/Comonadic.hs
@@ -6,27 +6,37 @@
 module Comonadic where
 
 import Control.Comonad
--- import Control.Comonad.Store
+import Data.Functor.Foldable
+import Control.Comonad.Store
 import qualified Data.List.NonEmpty as NE
 import Prelude
+import Safe (headMay)
 
-data Store s a = Store (s -> a) s
-instance (Show s, Show a) => Show (Store s a) where
-   show (Store f a) = "Store (\\" ++ show a ++  " -> " ++ show (f a) ++  " ) " ++ show a
+-- data StoreT s w a = StoreT (w (s -> a)) s
 
-instance Functor (Store s) where
-  fmap f (Store f' s) = Store (fmap f f') s
+-- instance (Show s, Show a) => Show (StoreT s w a) where
+--    show (StoreT f a) = "StoreT (\\" ++ show a ++  " -> " ++ show (f a) ++  " ) " ++ show a
 
-instance Comonad (Store s) where
-  extract :: Store s a -> a
-  extract (Store f s) = f s
+-- instance (Show s, Show a) => Show (Store s a) where
+--    show (Store f a) = "Store (\\" ++ show a ++  " -> " ++ show (f a) ++  " ) " ++ show a
 
-  extend :: forall a b. (Store s a -> b) -> Store s a -> Store s b
-  extend f' st@(Store _ a) = Store (\_ -> f' st) a
+-- data Store s a = Store (s -> a) s
 
-  duplicate :: forall a. Store s a -> Store s (Store s a)
-  duplicate (Store f a) = Store (\x -> Store f x) a
+-- instance Functor (Store s) where
+--   fmap f (Store f' s) = Store (fmap f f') s
 
+-- instance Comonad (Store s) where
+--   extract :: Store s a -> a
+--   extract (Store f s) = f s
+
+--   extend :: forall a b. (Store s a -> b) -> Store s a -> Store s b
+--   extend f' st@(Store _ a) = Store (\_ -> f' st) a
+
+--   duplicate :: forall a. Store s a -> Store s (Store s a)
+--   duplicate (Store f a) = Store (\x -> Store f x) a
+
+-- store :: (s -> a) -> s -> Store s a
+-- store f s = Store f s
 
 takeSome :: Int -> NE.NonEmpty Int -> NE.NonEmpty Int
 takeSome n input = NE.fromList $ NE.take n input
@@ -34,8 +44,12 @@ takeSome n input = NE.fromList $ NE.take n input
 ix :: Int -> NE.NonEmpty Int -> NE.NonEmpty Int
 ix index list = NE.fromList . snd $ NE.splitAt index list
 
--- neToStore :: NE.NonEmpty Int -> Store Int (Maybe Int)
--- neToStore l =
---   store (\i -> head $ NE.filter (\x -> x == i) l) 0
+neToStore :: NE.NonEmpty Int -> Store Int Int
+neToStore l =
+  let indexes = [0 .. NE.length l]
+    in
+      store (\x -> l NE.!! x) 0
 
--- extend (takeSome 2) fromList [1..]
+neToStore' :: NE.NonEmpty Int -> Store Int Int
+neToStore' (a NE.:| as) =
+  (store (\x -> a) a) <> neToStore' as
